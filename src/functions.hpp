5f00b5e188ab15381dda42a116a6a1689aa24d20
Evsyukov Denis Anatolyevich <d.evsyukov@tinkoff.ru>
2020-01-09 15:16:04 +0300

    First commit

---
+++ src/functions.hpp
@@ -0,0 +1,22 @@
+//
+// Created by d.evsyukov on 09.01.2020.
+//
+
+#ifndef T_FUNCTIONS_HPP
+#define T_FUNCTIONS_HPP
+
+std::string &ltrim(std::string &str, const std::string &chars = "\t\n\v\f\r ") {
+    str.erase(0, str.find_first_not_of(chars));
+    return str;
+}
+
+std::string &rtrim(std::string &str, const std::string &chars = "\t\n\v\f\r ") {
+    str.erase(str.find_last_not_of(chars) + 1);
+    return str;
+}
+
+std::string &trim(std::string &str, const std::string &chars = "\t\n\v\f\r ") {
+    return ltrim(rtrim(str, chars), chars);
+}
+
+#endif //T_FUNCTIONS_HPP
---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+#include "cxxopts.hpp"
+#include "functions.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cxxopts::Options options("MyProgram", "One line description of MyProgram");
+    options
+            .allow_unrecognised_options()
+            .add_options()
+                    ("positional",
+                     "Positional arguments: these are the arguments that are entered "
+                     "without an option", cxxopts::value<std::vector<std::string>>())
+                    ("e,edit", "edit TASK to contain TEXT", cxxopts::value<std::string>())
+                    ("f,finish", "mark TASK as finished", cxxopts::value<std::string>())
+                    ("r,remove", "Remove TASK from list", cxxopts::value<std::string>())
+                    ("l,list", "work on LIST", cxxopts::value<std::string>())
+                    ("t,taskdir", "work on the lists in DIR", cxxopts::value<std::string>())
+                    ("d,delete-if-empty", "delete the task file if it becomes empty")
+                    ("g,grep", "print only tasks that contain WORD", cxxopts::value<std::string>())
+                    ("v,verbose", "print more detailed output (full task ids, etc)")
+                    ("q,quiet", "print less detailed output (no task ids, etc)")
+                    ("D,done", "list done tasks instead of unfinished ones")
+                    ("h,help", "HELP");
+    options.parse_positional({"positional"});
+    auto result = options.parse(argc, argv);
+    const auto &arguments = result.arguments();
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        exit(0);
+    }
+    if (result.count("positional")) {
+        std::cout << "Positional = {" << std::endl;
+        std::string str;
+        auto &v = result["positional"].as<std::vector<std::string>>();
+        for (const auto &s : v) {
+            str += s + " ";
+        }
+        std::cout << "\"" << trim(str) << "\"" << std::endl << "}" << std::endl;
+    }
+    std::string taskdir;
+    if  (result.count("taskdir")) {
+        taskdir = result["taskdir"].as<std::string>();
+    } else {
+        namespace fs = std::filesystem;
+        taskdir=fs::current_path();
+    }
+
+    std::cout << "Current path is " << taskdir << '\n';
+    std::cout << "Saw " << arguments.size() << " arguments" << std::endl;
+    return 0;
+}
